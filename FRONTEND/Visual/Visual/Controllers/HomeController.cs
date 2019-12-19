8dfad94550207ddc886966d6e477ff99f4cf5b32
Fabian3830 <49417624+Fabian3830@users.noreply.github.com>
2019-12-19 11:18:17 -0600

    lel

--- FRONTEND/Visual/Visual/Controllers/HomeController.cs
+++ FRONTEND/Visual/Visual/Controllers/HomeController.cs
@@ -17,9 +17,9 @@ namespace PaginaVisual.Controllers
 
             using (var client = new HttpClient())
             {
-                client.BaseAddress = new Uri("http://localhost:44311/api/");
+                client.BaseAddress = new Uri("https://localhost:44311/api/");
                 //HTTP GET
-                var responseTask = client.GetAsync("Receta");
+                var responseTask = client.GetAsync("recetas/GetAll");
                 responseTask.Wait();
 
                 var result = responseTask.Result;
@@ -70,14 +70,38 @@ namespace PaginaVisual.Controllers
             return View();
         }
 
-        public ActionResult service()
+        public ActionResult Recetas()
         {
-            ViewBag.Message = "Your service page.";
+            IEnumerable<RecetasViewModel> recetas = null;
 
-            return View();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44311/api/");
+                //HTTP GET
+                var responseTask = client.GetAsync("recetas/GetAll");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<RecetasViewModel>>();
+                    readTask.Wait();
+
+                    recetas = readTask.Result;
+                }
+                else //web api sent error response 
+                {
+                    //log response status here..
+
+                    recetas = Enumerable.Empty<RecetasViewModel>();
+
+                    ModelState.AddModelError(string.Empty, "Server error. Please contact administrator.");
+                }
+            }
+            return View(recetas);
         }
 
-        public ActionResult single()
+        public ActionResult Productos()
         {
             ViewBag.Message = "Your single page.";
 
