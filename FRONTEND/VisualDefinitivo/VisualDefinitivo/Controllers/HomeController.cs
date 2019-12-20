e3a663c47e80fc0e219c1ee3486f7a3c68a625f1
JAFTSIX <42688559+JAFTSIX@users.noreply.github.com>
2019-12-20 10:01:15 -0600

    s
    s

--- FRONTEND/VisualDefinitivo/VisualDefinitivo/Controllers/HomeController.cs
+++ FRONTEND/VisualDefinitivo/VisualDefinitivo/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -106,6 +107,86 @@ namespace PaginaVisual.Controllers
             ViewBag.Message = "Your single page.";
 
             return View();
+        }
+
+
+        public ActionResult loguearse()
+        {
+            return View();
+        }
+
+ 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult loguearse(ClientesViewModel cliente)
+        {
+
+            
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://apicocina.azurewebsites.net/api/");
+
+                //HTTP POST
+                var postTask = client.PostAsJsonAsync<ClientesViewModel>("operations/login", cliente);
+                var lavuelta = postTask.Result.Content.ReadAsStringAsync();
+
+                JObject jObject = JObject.Parse(lavuelta.Result);
+                
+                
+                cliente.id_Cliente= Int32.Parse((string)jObject["id_Cliente"]);
+                cliente.iEdad = Int32.Parse((string)jObject["iEdad"]);
+                cliente.id_direccion = Int32.Parse((string)jObject["id_direccion"]);
+                cliente.vNombre = (string)jObject["vNombre"];
+                cliente.vApellido= (string)jObject["vApelldo"];
+                cliente.vApelldo2= (string)jObject["vApelldo2"];
+                cliente.vROL= (string)jObject["vROL"];
+
+                AutoMapper.Mapper.Initialize(cfg=> {
+                    cfg.CreateMap<ClientesViewModel, VisualDefinitivo.Models.Cliente>();
+                    cfg.CreateMap< VisualDefinitivo.Models.Cliente,ClientesViewModel > ();
+                });
+
+                VisualDefinitivo.Models.Cliente.INSTANCIA = AutoMapper.Mapper.Map<ClientesViewModel, VisualDefinitivo.Models.Cliente>(cliente);
+
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+
+                    if (cliente.vNombre!="")
+                    {
+                        Session["login"] = "si";
+                        return RedirectToAction("Index");
+                    }
+                    
+
+
+                    
+                }
+                else { 
+                    Session["login"] = "no";
+
+
+                   // return RedirectToAction("Index");
+                }
+
+
+
+            }
+
+            //ModelState.AddModelError(string.Empty, "Server Error. Please contact administrator.");
+
+            return View(cliente);
+        }
+
+
+
+        public ActionResult logout() {
+
+
+            Session["login"] = "no";
+            VisualDefinitivo.Models.Cliente.INSTANCIA = null;
+
+            return RedirectToAction("Index");
         }
     }
 }
---
+++ FRONTEND/VisualDefinitivo/VisualDefinitivo/Models/Cliente.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VisualDefinitivo.Models
+{
+    public class Cliente
+    {
+
+
+        private static Cliente Instancia = null;
+
+        public static Cliente INSTANCIA
+        {
+            get
+            {
+                if (Instancia == null)
+                {
+                    return new Cliente();
+                }
+                return Instancia;
+            }
+            set
+            {
+                if (Instancia == null)
+                {
+                    Instancia = value;
+                }
+            }
+        }
+
+        public int id_Cliente { get; set; }
+        public string vNombre { get; set; }
+        public string vApellido { get; set; }
+        public string vApelldo2 { get; set; }
+        public string vContra { get; set; }
+        public Nullable<int> iEdad { get; set; }
+        public int id_direccion { get; set; }
+        public string vROL { get; set; }
+        public string vCorreo { get; set; }
+    }
+}
